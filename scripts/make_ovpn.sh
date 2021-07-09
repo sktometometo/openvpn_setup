1c5a5547c311381e01ca637b1762296841d1554d
Koki Shinjo <sktometometo@gmail.com>
2021-07-09 15:00:54 +0900

    make_ovpn.sh

---
+++ scripts/make_ovpn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FILE_OUTPUT=${1}
+FILE_BASE_CONFIG=${2}
+FILE_CA_CRT=${3}
+FILE_CLIENT_CRT=${4}
+FILE_CLIENT_KEY=${5}
+FILE_TLS_KEY=${6}
+
+
+cat ${FILE_BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${FILE_CA_CRT} \
+    <(echo -e '</ca>\n<cert>') \
+    ${FILE_CLIENT_CRT} \
+    <(echo -e '</cert>\n<key>') \
+    ${FILE_CLIENT_KEY} \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${FILE_TLS_KEY} \
+    <(echo -e '</tls-crypt>') \
+    > ${FILE_OUTPUT}
